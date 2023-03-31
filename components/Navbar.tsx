f628ef7c848f0cbffcfbafa46ca83b7a0b14e2d5
Bright Iortsor <iortsor@gmail.com>
2023-03-31 10:09:07 +0100

    added color palette in cofig folder

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,50 @@
+import { Box } from "@mui/material";
+import Typography from "@mui/material/Typography";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flex: 1,
+          backgroundColor: "aqua",
+        }}
+      >
+        <Box>111</Box>
+        <Box sx={{ marginLeft: "1.5rem" }}>
+          <Typography
+            variant="h6"
+            sx={{ fontSize: "0.9rem", fontWeight: 600, marginBottom: "-2px" }}
+          >
+            Vita Corner Cafe
+          </Typography>
+          <Typography variant="body2" sx={{ color: "gray" }}>
+            Inventory
+          </Typography>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          flex: 3,
+          backgroundColor: "teal",
+        }}
+      >
+        2
+      </Box>
+      <Box
+        sx={{
+          flex: 1,
+          backgroundColor: "orange",
+        }}
+      >
+        3
+      </Box>
+    </Box>
+  );
 };
 export default Navbar;
--- config/colors.ts
+++ config/colors.ts
@@ -1,1 +1,29 @@
-export const red100 = "red";
+export const white = "#ffffff";
+
+export const gray50 = "#f9f9f9";
+export const gray100 = "#f5f5f5";
+export const gray200 = "#ebebeb";
+export const gray300 = "#e1e1e1";
+export const gray400 = "#d7d7d7";
+export const gray500 = "#cccccc";
+export const gray600 = "#c2c2c2";
+
+export const green100 = "#a5e2a5";
+export const green200 = "#8cd98c";
+export const green300 = "#75d075";
+
+//To be used as bg for menu Items
+export const orange50 = "#f9e2e2";
+export const orange100 = "#e2a5a5";
+export const orange200 = "#d98c8c";
+export const orange300 = "#d07575";
+
+export const purple100 = "#a5a5e2";
+export const purple200 = "#8c8cd9";
+export const purple300 = "#7575d0";
+
+export const black50 = "rgba(0, 0, 0, 0.05)";
+export const black100 = "rgba(0, 0, 0, 0.1)";
+export const black200 = "rgba(0, 0, 0, 0.2)";
+export const black300 = "rgba(0, 0, 0, 0.3)";
+export const black400 = "rgba(0, 0, 0, 0.4)";
